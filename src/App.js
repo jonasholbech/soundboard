ba4591055929cddd2a7837ef8a63ca6d67dba282
Jonas Holbech <jh@jonasholbech.dk>
2021-04-12 21:05:54 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useRef } from "react";
+import Appbar from "muicss/lib/react/appbar";
+import Button from "muicss/lib/react/button";
+const data = [
+  "airhorn",
+  "ding",
+  "halleluja",
+  "rimshot",
+  "trololo",
+  "x-files",
+  "fart",
+  "power-up",
+  "mexican-trumpet",
+  "wrong-answer",
+  "power-rangers",
+  "darth-vader-power",
+  "lightsaber",
+];
 function App() {
+  const audioEl = useRef(null);
+  function play(sound) {
+    audioEl.current.src = `sounds/${sound}.mp3`;
+    audioEl.current.currentTime = 0;
+    audioEl.current.play();
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Appbar>
+        <h1>SoundBoard</h1>
+      </Appbar>
+      <audio ref={audioEl}></audio>
+      {data.map((sound) => (
+        <Button key={sound} color="primary" onClick={() => play(sound)}>
+          {sound}
+        </Button>
+      ))}
     </div>
   );
 }
